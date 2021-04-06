a3e27b79ebe22b90aee37956a6bd4d3f2ded81b9
Takuma Ogawa <nyabo10_31@yahoo.co.jp>
2021-04-06 18:37:48 +0900

    Initial Commit

---
+++ FoldingMemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoldingMemoApp
+//
+//  Created by 小川卓馬 on 2021/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

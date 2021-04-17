ada95dd8e999354982f8bc3ab50cc6a5d643aa2d
Takuma Ogawa <nyabo10_31@yahoo.co.jp>
2021-04-17 11:52:29 +0900

    MemoDetailTableViewCell作成

---
+++ FoldingMemoApp/Views/MemoDetailTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MemoDetailTableViewCell.swift
+//  FoldingMemoApp
+//
+//  Created by 小川卓馬 on 2021/04/17.
+//
+
+import UIKit
+
+class MemoDetailTableViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

28796514fba616aed59a88820eb7ef7de40f87a5
Takuma Ogawa <nyabo10_31@yahoo.co.jp>
2021-04-18 09:45:20 +0900

    MemoDetailTableViewCellをテーブルビューに登録

--- FoldingMemoApp/Controllers/TitleListViewController.swift
+++ FoldingMemoApp/Controllers/TitleListViewController.swift
@@ -14,8 +14,21 @@ final class TitleListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.register(MemoDetailTableViewCell.nib(), forCellReuseIdentifier: MemoDetailTableViewCell.identifier)
     }
 
 
 }
 
+extension TitleListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MemoDetailTableViewCell.identifier, for: indexPath) as! MemoDetailTableViewCell
+        return cell
+    }
+    
+    
+}
